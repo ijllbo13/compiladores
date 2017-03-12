6e3b476856b0cdd58a6501b7d1bc48a5603804bc
ijllbo13 <ijllbo13@gmail.com>
2017-03-12 11:00:57 -0600

    Update AnalisisDescendente.py

--- Analisis Descendente/AnalisisDescendente.py
+++ Analisis Descendente/AnalisisDescendente.py
@@ -1,29 +1,43 @@
-pila = []
-token = ["id", "+" , "id"]
-matriz = {
+stack = []
+tokens = ["(","int","+","int",")","(", "int",")","$"]
+terminals = ["int", "+","*","(",")", "$"]
+nonTerminals = ["E","E'","T","T'", "F"]
+initial = "E"
+firstS = ["(", "int", "$"]
+
+matrix = {
     "E": {
-        "id" : "TE'" ,
-        "("  : "TE'"
+        "int" : ["T","E'"] ,
+        "("  : ["T","E'"],
+        ")"  : ["sync"],
+        "$"  : ["sync"]
     },
     "E'": {
-        "+"  : "TE'",
-        ")"  : "#",
-        "$"  : "#"
+        "+"  : ["+","T","E'"],
+        ")"  : ["eps"],
+        "$"  : ["eps"]
     },
     "T": {
-        "id" : "FT'",
-        "("  : "FT'"
+        "int" : ["F", "T'"],
+        "+"  : ["sync"],
+        "("  : ["F","T'"],
+        ")"  : ["sync"],
+        "$"  : ["sync"]
     },
     "T'" : {
-        "+"  : "#",
-        "*"  : "*FT'",
-        ")"  : "#",
-        "$"  : "#"
+        "+"  : ["eps"],
+        "*"  : ["*", "F", "T'"],
+        ")"  : ["eps"],
+        "$"  : ["eps"]
         },
     "F" : {
-        "id" : "id",
-        "("  : "(E)"
-    }
+        "int"  : ["int"],
+        "+"  : ["sync"],
+        "*"  : ["sync"],
+        "("  : ["(", "E", ")"],
+        ")"  : ["sync"],
+        "$"  : ["sync"]
+        }
 }
 
 
@@ -33,80 +47,80 @@ E'-> +TE' | eps
 T -> FU
 U -> *FU | eps
 F -> (E) | id
-
 """
+def isTerminal(symbol):
+    if(symbol in terminals):
+        return True
+    else:
+        return False
+
+def insertRule(top, token):
+    global stack
+    global matrix
+    reversedRule = reversed(matrix[top][token])
+    for value in reversedRule:
+        stack.append(value)
 
 
 ########################
 
 
-#analisis sintactico
+stack.append("$")
+stack.append("E")            #inicializar la stack
+coincidence = ''               #incializar coincidencias
 
-pila += "$"
-pila += "E"
 
-coincidencia = ''
+output = ["", "", "", ""]
 
-"""
-$          id+id$
-TE'$
-matriz[E][id]
-"""
+print("{:>16}\t{:>16}\t{:>16}\t{:<16}\n".format("Coincidencia", "stack", "Entrada", "Accion"))
 
-def predAnalysis(token):
-    while( pila[-1] != '$'):
-        print(coincidencia, end = " \t\t")
+del output[:]
 
-        for i in pila:
-            print(i, end = "")
+while(len(stack)):
+    output.append(coincidence)
 
-        print("\t\t", end = "")
+    output.append("".join(reversed(stack)))
 
-        for i in token:
-            print(i, end = "")
+    output.append("".join(tokens))
 
-        print("\t\t", end = "")
+    top = stack.pop()                            #se obtiene el primero de la stack
+    token = tokens[0]                        #se obtiene el token del principio
 
-        top = pila.pop()
-        nextToken = token[-1]
+    if(top == token):
+        tokens.pop(0)
+        coincidence += token
+        output.append("Coincidencia " + token)
 
-        if(top == nextToken):
-            token.pop()
-            coincidencia += nextToken
-            print('Coincidencia ' + nextToken)
+    elif(top == '$'):
+        # Pila vacía -> meter inicial a la pila y hacer pop en tokens hasta que encuentre uno que este en su first
+        stack.append("$")
+        stack.append(initial)
+        while(tokens[0] not in firstS):
+            tokens.pop(0)
+        output.append("Pila vacía, llenar hasta token in First(S)")
 
-        elif (not (nextToken in matriz[top])):
-            print('Error')
+    elif(token in matrix[top]):
+        if ("#" in matrix[top][token]):
+            output.append("Salida " + top + " -> #")
+        else: #Checar si es SYNC dentro de este else -> se omite el top
+            if "sync" in matrix[top][token]:
+                output.append("error:M[" + top + "," + token + "] = sync, " + "pop(" + top + ")")
+            elif "eps" in matrix[top][token]:
+                output.append("Salida " + top + " -> " + "".join(matrix[top][token]))
+            else:
+                insertRule(top, token)
+                output.append("Salida " + top + " -> " + "".join(matrix[top][token]))
 
-        elif (matriz[top][nextToken] != '#'):
-                pila += (matriz[top][nextToken])
-                print('Salida ' + top + ' ->' + matriz[top][nextToken])
-
-        elif (matriz[top][nextToken] == '#'):
-                continue
-
+    else: #Aqui se revisa si el error fue M[top,token] o si top != token
+        if token not in matrix[top]:
+            tokens.pop(0)
+            stack.append(top)
+            output.append("Error: Símbolo omitido")
         else:
-            print('Error')
+            output.append("pop(" + top + ")")
 
+    print("{:>16}\t{:>16}\t{:>16}\t{:<16}".format(output[0], output[1], output[2], output[3]))
+    del output[:]
 
-class Stack(object):
-
-    def __init__(self):
-        self.storage = []
-
-    def isEmpty(self):
-        return len(self.storage) == 0
-
-    def push(self,p):
-        self.storage[:0] = p
-
-    def pop(self):
-        """issue: throw exception?"""
-        return None
-
-
- $E              id+id           Salida E ->TE'
-                 $TE'            id+id           Traceback (most recent call last):
-  File "C:\Users\simio\Desktop\IDE´s Projects\Python\AnalisisDescendente.py", line 78, in <module>
-    elif (not (nextToken in matriz[top])):
-KeyError: "'"
+    if (len(stack) == 0):
+        print("Aceptado")
